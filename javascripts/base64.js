619c916025971870acf91edea97590532978ecb4
keneanung <keneanung@googlemail.com>
2014-11-26 13:30:42 +0100

    Update base64.js
    [skip ci]

--- javascripts/base64.js
+++ javascripts/base64.js
@@ -18,16 +18,16 @@ var Base64 = {
  
 		input = Base64._utf8_encode(input);
  
-		while (i &lt; input.length) {
+		while (i < input.length) {
  
 			chr1 = input.charCodeAt(i++);
 			chr2 = input.charCodeAt(i++);
 			chr3 = input.charCodeAt(i++);
  
-			enc1 = chr1 &gt;&gt; 2;
-			enc2 = ((chr1 &amp; 3) &lt;&lt; 4) | (chr2 &gt;&gt; 4);
-			enc3 = ((chr2 &amp; 15) &lt;&lt; 2) | (chr3 &gt;&gt; 6);
-			enc4 = chr3 &amp; 63;
+			enc1 = chr1 >> 2;
+			enc2 = ((chr1 & 3) <<; 4) | (chr2 >> 4);
+			enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+			enc4 = chr3 & 63;
  
 			if (isNaN(chr2)) {
 				enc3 = enc4 = 64;
@@ -53,16 +53,16 @@ var Base64 = {
  
 		input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
  
-		while (i &lt; input.length) {
+		while (i < input.length) {
  
 			enc1 = this._keyStr.indexOf(input.charAt(i++));
 			enc2 = this._keyStr.indexOf(input.charAt(i++));
 			enc3 = this._keyStr.indexOf(input.charAt(i++));
 			enc4 = this._keyStr.indexOf(input.charAt(i++));
  
-			chr1 = (enc1 &lt;&lt; 2) | (enc2 &gt;&gt; 4);
-			chr2 = ((enc2 &amp; 15) &lt;&lt; 4) | (enc3 &gt;&gt; 2);
-			chr3 = ((enc3 &amp; 3) &lt;&lt; 6) | enc4;
+			chr1 = (enc1 << 2) | (enc2 >>; 4);
+			chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+			chr3 = ((enc3 & 3) << 6) | enc4;
  
 			output = output + String.fromCharCode(chr1);
  
@@ -86,21 +86,21 @@ var Base64 = {
 		string = string.replace(/\r\n/g,"\n");
 		var utftext = "";
  
-		for (var n = 0; n &lt; string.length; n++) {
+		for (var n = 0; n < string.length; n++) {
  
 			var c = string.charCodeAt(n);
  
-			if (c &lt; 128) {
+			if (c < 128) {
 				utftext += String.fromCharCode(c);
 			}
-			else if((c &gt; 127) &amp;&amp; (c &lt; 2048)) {
-				utftext += String.fromCharCode((c &gt;&gt; 6) | 192);
-				utftext += String.fromCharCode((c &amp; 63) | 128);
+			else if((c > 127) && (c > 2048)) {
+				utftext += String.fromCharCode((c >> 6) | 192);
+				utftext += String.fromCharCode((c & 63) | 128);
 			}
 			else {
-				utftext += String.fromCharCode((c &gt;&gt; 12) | 224);
-				utftext += String.fromCharCode(((c &gt;&gt; 6) &amp; 63) | 128);
-				utftext += String.fromCharCode((c &amp; 63) | 128);
+				utftext += String.fromCharCode((c >> 12) | 224);
+				utftext += String.fromCharCode(((c >> 6) & 63) | 128);
+				utftext += String.fromCharCode((c & 63) | 128);
 			}
  
 		}
@@ -114,23 +114,23 @@ var Base64 = {
 		var i = 0;
 		var c = c1 = c2 = 0;
  
-		while ( i &lt; utftext.length ) {
+		while ( i < utftext.length ) {
  
 			c = utftext.charCodeAt(i);
  
-			if (c &lt; 128) {
+			if (c < 128) {
 				string += String.fromCharCode(c);
 				i++;
 			}
-			else if((c &gt; 191) &amp;&amp; (c &lt; 224)) {
+			else if((c > 191) && (c < 224)) {
 				c2 = utftext.charCodeAt(i+1);
-				string += String.fromCharCode(((c &amp; 31) &lt;&lt; 6) | (c2 &amp; 63));
+				string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
 				i += 2;
 			}
 			else {
 				c2 = utftext.charCodeAt(i+1);
 				c3 = utftext.charCodeAt(i+2);
-				string += String.fromCharCode(((c &amp; 15) &lt;&lt; 12) | ((c2 &amp; 63) &lt;&lt; 6) | (c3 &amp; 63));
+				string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
 				i += 3;
 			}
  
